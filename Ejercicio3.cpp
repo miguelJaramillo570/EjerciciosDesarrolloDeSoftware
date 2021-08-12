152a004858ca8c78411258279bec848db4075127
miguelJaramillo570 <88807967+miguelJaramillo570@users.noreply.github.com>
2021-08-11 20:08:14 -0500

    Update Ejercicio3.cpp

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -9,10 +9,10 @@ int main(){
 char *fun(char *cad1, char *cad2)
 {
  char *aux; int tam;
- tam = strlen(cad1)+strlen(cad2)+1; // +1 para el caracter \0 final
- aux = (char *)malloc(tam); //Crear la nueva cadena
+ tam = strlen(cad1)+strlen(cad2)+1;
+ aux = (char *)malloc(tam);
  if (aux!=NULL)
- { strcpy(aux,cad1); strcat(aux,cad2); } //Copiar cad1+cad2 en aux
+ { strcpy(aux,cad1); strcat(aux,cad2); } 
  return aux;
 }
 }
